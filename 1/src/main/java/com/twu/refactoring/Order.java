783e19c5686707c7f63fde57070dade541a5b6ef
Zhai-Chao <707295902@qq.com>
2021-08-08 18:31:48 +0800

    practice 1

--- 1/src/main/java/com/twu/refactoring/Order.java
+++ 1/src/main/java/com/twu/refactoring/Order.java
@@ -3,25 +3,49 @@ package com.twu.refactoring;
 import java.util.List;
 
 public class Order {
-    String nm;
-    String addr;
-    List<LineItem> li;
+    private final String customerName;
+    private final String customerAddress;
+    private final List<LineItem> lineItems;
 
-    public Order(String nm, String addr, List<LineItem> li) {
-        this.nm = nm;
-        this.addr = addr;
-        this.li = li;
+    public Order(String customerName, String customerAddress, List<LineItem> lineItems) {
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.lineItems = lineItems;
     }
 
     public String getCustomerName() {
-        return nm;
+        return customerName;
     }
 
     public String getCustomerAddress() {
-        return addr;
+        return customerAddress;
     }
 
     public List<LineItem> getLineItems() {
-        return li;
+        return lineItems;
+    }
+
+    public String getPrintLine() {
+        StringBuilder printLines = new StringBuilder();
+        for (LineItem lineItem : lineItems) {
+            printLines.append(lineItem.getDescription());
+            printLines.append('\t');
+            printLines.append(lineItem.getPrice());
+            printLines.append('\t');
+            printLines.append(lineItem.getQuantity());
+            printLines.append('\t');
+            printLines.append(lineItem.totalAmount());
+            printLines.append('\n');
+        }
+        return printLines.toString();
+    }
+
+    public Double getTotalTax() {
+        return lineItems.stream().mapToDouble(i -> i.totalAmount() * .10).sum();
+    }
+
+    public Double getTotalAmount() {
+        return lineItems.stream().mapToDouble(LineItem::totalAmount).sum() + getTotalTax();
     }
 }
+
